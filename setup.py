fc74df636e65c86414d14c32209242ef601db919
Adam <adam@iris.washington.edu>
2015-04-22 12:54:08 -0700

    Added setup info

---
+++ setup.py
@@ -0,0 +1,30 @@
+# coding:utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+
+setup(name='django-lib',
+      version='0.0.1',
+      description='IRIS DMC common library',
+      long_description=open('README.rst').read(),
+      classifiers=[],
+      keywords='',
+      author='Adam Clark',
+      author_email='adam@iris.washington.edu',
+      url='http://ds.iris.edu/',
+      license='Apache 2',
+      packages=['lib'],
+      include_package_data=True,
+      zip_safe=True,
+      tests_require=[
+          'nose',
+          'mock'
+      ],
+      install_requires=[
+          'Django>=1.6.8',
+          'textile>=2.2.1',
+          'iso3166>=0.6',
+          'requests>=2.2.1',
+      ],
+      )
