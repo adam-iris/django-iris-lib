bb2f8e986a091e8634341f050932e3cf39ec90c6
adam <adam@1916f7c8-7579-4fb5-afaa-ad426bfb3a17>
2015-06-05 17:59:24 +0000

    Start of webservice example
    git-svn-id: svn+ssh://svn1.iris.washington.edu/svn_repository/iris_repos/django_iris_lib/trunk@12444 1916f7c8-7579-4fb5-afaa-ad426bfb3a17

--- examples/urls.py
+++ examples/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from examples.coordinates import CoordinatesView
+from examples.webservices import WebserviceView
 
 urlpatterns = patterns('',
     url(r'coordinates/', CoordinatesView.as_view()),
+    url(r'ws/', WebserviceView.as_view()),
 )
